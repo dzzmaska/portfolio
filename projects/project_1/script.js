0b5be023b9e3d70b95820e002ad7b653804123cc
dzzmaska <63068151+dzzmaska@users.noreply.github.com>
2024-10-25 21:49:58 +0200

    Add files via upload

---
+++ projects/project_1/script.js
@@ -0,0 +1,29 @@
+
+/*console.log(12>10);
+console.log(10>12);
+
+console.log(12>=10);
+console.log(10<=10);
+
+console.log(12==12);
+console.log(10=="10");//true
+console.log(null==undefined);
+console.log(0=='');
+console.log(false==0);
+console.log(true==1);
+*/
+
+var color="red"
+var color2="";
+if(color==="blue"){
+    color2="Blue"
+}else if(color==="green"){
+    color2="green"
+}else if(color==="white"){
+    color2="white"
+}else{
+    console.log("There are no such colors in here");
+}
+console.log("Color is "+color2);
+
+
